2068873a0ad871ddb522ae542d4771a1a8bc026c
nils <nillenice45@gmail.com>
2019-08-26 10:36:00 +0200

    Update övning2.java

--- Protein2/Repetitions_uppgifter/övning2.java
+++ Protein2/Repetitions_uppgifter/övning2.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class övning2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
+		String ord1 = sc.nextLine();
+		String ord2 = sc.nextLine();
+		String ord3 = sc.nextLine();
 		
+		System.out.println(ord3 +" "+ ord2 +" "+ ord1);
 	}
 }
